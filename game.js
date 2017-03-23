6bcb2e8c7182028c8da083b07a7d76d3d39f21fd
seanrgbriggs <seanrgbriggs@gmail.com>
2017-03-23 18:50:39 -0400

    Switched bead light detection over to bitwise for coolness. lowered tempo for debugging. Switched colors over to brighter variants instead of the rainbow. Moved G.BG_COL to exports.

--- game.js
+++ game.js
@@ -17,28 +17,39 @@ You may have received a copy of the GNU Lesser General Public License
 along with Perlenspiel. If not, see <http://www.gnu.org/licenses/>.
 */
 
+// game.js for Perlenspiel 3.2.x
+// This is a template for creating new Perlenspiel games
+
+// The following comment lines are for JSLint/JSHint. Don't remove them!
+
 // The "use strict" directive in the following line is important. Don't remove it!
 "use strict";
-
 /*jslint nomen: true, white: true */
 /*global PS */
 
+//var LIT_COLORS = [0xff0080, 0xff0000, 0xff8000, 0xffff00,
+  //  0x80ff00, 0x00ff00, 0x00ff80, 0x00ffff,
+////0x0080ff, 0x0000ff, 0x8000ff, 0xff00ff];
 
 var G= (function () {
     var MIDDLE_C = 60;
-    var BG_COL = 0x303030;
+    var BG_COL = PS.COLOR_BLACK;
 	var HIGHLIGHT_COL = 0xF0F0F0;
     var BEAD_RADIUS = 20;
-	var MAX_WIDTH = 31;
-    var HEIGHT = 12;
-	var LIT_COLORS = [0xff0080, 0xff0000, 0xff8000, 0xffff00, 0x80ff00, 0x00ff00, 0x00ff80, 0x00ffff, 0x0080ff, 0x0000ff, 0x8000ff, 0xff00ff];
+	var MAX_WIDTH = 32;
+	var LIT_COLORS = [0xff0080, 0xff0000, 0xff8000, 0xffff00,
+        0x80ff00, 0x00ff00];
+    LIT_COLORS = LIT_COLORS.concat(LIT_COLORS.reverse());
+
     var UNLIT_COLORS = LIT_COLORS.map(function (col) {
 		function dim(col, mask) {
 			return ((col & mask) / 2) & mask;
         }
 		return dim(col, 0xff)+dim(col,0xff00)+dim(col,0xff0000);
     })
-	var PLAY_BUTTON = {x:16, y:HEIGHT, playglyph:"▶", pauseglyph:"▌ ▌"};
+    var HEIGHT = LIT_COLORS.length;
+
+    var PLAY_BUTTON = {x:16, y:HEIGHT, playglyph:"▶", pauseglyph:"▌ ▌"};
 
 
 	var width, columns, currentColumn, tempo, octave,tempoTimerPtr;
@@ -47,7 +58,7 @@ var G= (function () {
     for(var q = 0; q < 31; q++){
             columns.push(0);
 	}
-	tempo = 5;
+	tempo = 50;
 	currentColumn = -1;
 
 
@@ -97,10 +108,16 @@ var G= (function () {
         var y;
         for(y = 0; y<HEIGHT; y++){
             //if the square is lit
+
+            if(columns[currentColumn] & (1 << y)) {
+                PS.debug(columns[currentColumn] ^ (1 << y)+"\n");
+                PS.audioPlay(PS.piano(MIDDLE_C + y));
+            }
+            /*
             if(LIT_COLORS.indexOf(PS.color(currentColumn, y)) !== -1){
                 //PS.debug(y+"\n");
                 PS.audioPlay( PS.piano( MIDDLE_C + y ) );
-            }
+            }*/
         }
     }
 
@@ -139,7 +156,9 @@ var G= (function () {
 			MAX_WIDTH:MAX_WIDTH,
             LIT_COLORS:LIT_COLORS,
 			UNLIT_COLORS:UNLIT_COLORS,
-			HEIGHT:HEIGHT
+			HEIGHT:HEIGHT,
+
+            BG_COL:BG_COL
 		},
 		GridIterator:GridIterator,
 		switchBead:switchBead,
@@ -147,18 +166,6 @@ var G= (function () {
 		pausePlay:pausePlay
 	};
 }());
-PS.init = function( system, options ) {
-
-	PS.gridSize( G.constants.MAX_WIDTH, G.constants.HEIGHT + 1 );
-    PS.gridColor (PS.COLOR_GRAY);
-    for(var gi = new G.GridIterator(G.constants.MAX_WIDTH, G.constants.HEIGHT); !gi.isDone(); gi.next()){
-
-// Add any other initialization code you need here
-// Use PS.gridSize( x, y ) to set the grid to
-// the initial dimensions you want (32 x 32 maximum)
-// Do this FIRST to avoid problems!
-// Otherwise you will get the default 8x8 grid
-// This is a template for creating new Perlenspiel games
 
 // All of the functions below MUST exist, or the engine will complain!
 
@@ -169,16 +176,22 @@ PS.init = function( system, options ) {
 // [system] = an object containing engine and platform information; see documentation for details
 // [options] = an object with optional parameters; see documentation for details
 
+// Add any other initialization code you need here
+// Use PS.gridSize( x, y ) to set the grid to
+// Otherwise you will get the default 8x8 grid
+// the initial dimensions you want (32 x 32 maximum)
+// Do this FIRST to avoid problems!
 
-// game.js for Perlenspiel 3.2.x
+PS.init = function( system, options ) {
 
-// The following comment lines are for JSLint/JSHint. Don't remove them!
-//DEBUG STATEMENT REMOVE LATER
+	PS.gridSize( G.constants.MAX_WIDTH, G.constants.HEIGHT + 1 );
+    PS.gridColor (G.BG_COL);
+    for(var gi = new G.GridIterator(G.constants.MAX_WIDTH, G.constants.HEIGHT); !gi.isDone(); gi.next()){
         PS.color(gi.x, gi.y, G.constants.UNLIT_COLORS[gi.y]);
     }
     PS.alpha (PS.ALL, PS.ALL, 60);
     G.pausePlay();
-	PS.border(PS.ALL, G.constants.HEIGHT, 0);
+    PS.border(PS.ALL, G.constants.HEIGHT, 0);
 };
 
 
@@ -190,6 +203,7 @@ PS.init = function( system, options ) {
 // [data] = the data value associated with this bead, 0 if none has been set
 // [options] = an object with optional parameters; see documentation for details
 
+
 PS.touch = function( x, y, data, options ) {
 	// Uncomment the following line to inspect parameters
 	PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
@@ -200,6 +214,7 @@ PS.touch = function( x, y, data, options ) {
 	}
 	// Add code here for mouse clicks/touches over a bead
 };
+
 
 // PS.release ( x, y, data, options )
 // Called when the mouse button is released over a bead, or when a touch is lifted off a bead
